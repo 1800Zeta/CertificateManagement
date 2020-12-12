74f50bc6f0d90d351b48daa9fa40c05369f61ee4
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 17:53:48 +0000

    Update GetAndDeploy.ps1
    Fix module detection

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -1,8 +1,9 @@
 
-try{
-    Get-Module Posh-ACME
-    Write-Output "Posh-ACME module is installed"
-}
-catch {
+$ModuleInstalled = Get-Module Posh-ACME
+if($null -eq $ModuleInstalled){
+    Write-Output "Posh-ACME module is NOT installed, proceeding with install"
     Install-Module Posh-ACME -SkipPublisherCheck
+}
+else {
+    Write-Output "Posh-ACME module is installed"
 }
