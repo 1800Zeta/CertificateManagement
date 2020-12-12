6b134d70f745fddafc7661cfc163e1b8d6361371
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 18:03:56 +0000

    Update GetAndDeploy.ps1

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -2,7 +2,7 @@
 $ModuleInstalled = Get-Module Posh-ACME
 if($null -eq $ModuleInstalled){
     Write-Output "Posh-ACME module is NOT installed, proceeding with install"
-    Install-Module Posh-ACME -SkipPublisherCheck
+    Install-Module Posh-ACME -Confirm:$false
 }
 else {
     Write-Output "Posh-ACME module is installed"
