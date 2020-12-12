08dad6d690d51d417bb867a1b04aa38921faa25a
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 22:03:33 +0000

    Update GetAndDeploy.ps1

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -1,16 +1,15 @@
-$psRepository = Get-PSRepository -Name PSGallery
-if($psRepository.InstallationPolicy -eq "Untrusted"){
-    Write-Output "PSGallery is Untrusted"
-    Set-PSRepository $psRepository -InstallationPolicy Trusted
+$nugetVersion = Get-PackageProvider -Name NuGet
+if($nugetVersion.Version -lt "3.0.0.1"){
+    Write-Output "NuGet needs an update"
 }
 else {
-    Write-Output "PSGallery is trusted"
+    Write-Output "NuGet is up to date"
 }
 
 $moduleInstalled = Get-Module Posh-ACME
 if($null -eq $moduleInstalled){
     Write-Output "Posh-ACME module is NOT installed, proceeding with install"
-    Install-Module Posh-ACME -Force -Scope CurrentUser -SkipPublisherCheck
+    Install-Module Posh-ACME -Force -Scope CurrentUser
 }
 else {
     Write-Output "Posh-ACME module is installed"
