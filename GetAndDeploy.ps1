12dd242e1bb44f121a44b835dc038367140fbc00
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 21:44:59 +0000

    Update GetAndDeploy.ps1

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -1,6 +1,14 @@
+$psRepository = Get-PSRepository -Name PSGallery
+if($psRepository.InstallationPolicy -eq "Untrusted"){
+    Write-Output "PSGallery is Untrusted"
+    Set-PSRepository $psRepository -InstallationPolicy Trusted
+}
+else {
+    Write-Output "PSGallery is trusted"
+}
 
-$ModuleInstalled = Get-Module Posh-ACME
-if($null -eq $ModuleInstalled){
+$moduleInstalled = Get-Module Posh-ACME
+if($null -eq $moduleInstalled){
     Write-Output "Posh-ACME module is NOT installed, proceeding with install"
     Install-Module Posh-ACME -Force -Scope CurrentUser -SkipPublisherCheck
 }
