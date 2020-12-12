762b3227e5421fc55f6c732f3fd959fa1defcb4e
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 23:38:04 +0000

    Update GetAndDeploy.ps1

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -13,13 +13,16 @@ $moduleInstalled = Get-Module Posh-ACME
 if($null -eq $moduleInstalled){
     Write-Output "Posh-ACME module is NOT installed, proceeding with install"
     Install-Module Posh-ACME -Force
-}
-else {
+} else {
     Write-Output "Posh-ACME module is installed"
 }
 
 $configJSON = Get-Content CertificateConfig.json | ConvertFrom-Json
 
+$cfTokenSecure = ConvertTo-SecureString $configJSON.cloudflareToken -AsPlainText -Force
+$cfPluginArgs = @{ CFToken = $cfTokenSecure}
+
+New-PACertificate -Domain $configJSON.certificateNames -DnsPlugin Cloudflare -PluginArgs $cfPluginArgs -AcceptTOS -Contact $configJSON.emailNotifications
 Write-Output "Certificate Names are " $configJSON.certificateNames
 Write-Output "Emails sent to " $configJSON.emailNotifications
 Write-Output "Cloudflare token " $configJSON.cloudflareToken
