353dd754bded533c6c4053ab2a4f6ed5fc1716af
Mark Lewis <mark.lewis@jouzu.co.uk>
2020-12-12 22:17:50 +0000

    Update GetAndDeploy.ps1

--- GetAndDeploy.ps1
+++ GetAndDeploy.ps1
@@ -1,4 +1,12 @@
-$nugetVersion = Get-PackageProvider -Name NuGet
+try {
+    $nugetVersion = Get-PackageProvider -Name NuGet    
+}
+catch {
+    Write-Output "NuGet is not installed"
+    Install-PackageProvider NuGet -Force
+    $nugetVersion = Get-PackageProvider -Name NuGet
+}
+
 if($nugetVersion.Version -lt "3.0.0.1"){
     Write-Output "NuGet needs an update"
 }
